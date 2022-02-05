0200e01602eb63315d6e194723610b05d6ebc42c
johnathanf@bitbucket.com <johnathan_fisher93@yahoo.co.uk>
2022-02-05 03:18:53 +0000

    Add client

---
+++ clients/game/src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import leaflet from 'leaflet';
+import Phaser from 'phaser';
+
+console.log('Leaflet:', leaflet);
+console.log('Phaser:', Phaser);
+
+ReactDOM.render(
+    <React.StrictMode>
+        <div />
+    </React.StrictMode>,
+    document.getElementById('root'),
+);
